9b97888ce297d6cc4d37f4ae007414170748ed4e
swyx <shawnthe1@gmail.com>
2016-11-27 06:56:58 +0000

    completed

--- project.js
+++ project.js
@@ -1,10 +1,21 @@
 var express = require('express')
 var app = express()
 
+/*
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+*/
+app.get("/:text", function(req, res) {
+    var dt = new Date(req.params.text)
+    if (dt.toDateString() == "Invalid Date"){
+            res.end(JSON.stringify({ "unix": null, "natural": null }));
+    } else {
+        res.end(JSON.stringify({ "unix": dt.getTime()/1000, "natural": dt.toDateString() }));
+    }
+    
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
